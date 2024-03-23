fed281d4759dd40d7f03951eddac5b9b3b563046
Schesch <57675828+Schesch@users.noreply.github.com>
2024-03-23 18:10:20 +0100

    Update 2_📈_Laborfond.py

--- pages/2_📈_Laborfond.py
+++ pages/2_📈_Laborfond.py
@@ -134,7 +134,7 @@ if sprache == 'Deutsch':
 
 
 
-    excel_path = 'data/Laborfond.xlsx'
+    excel_path = 'https://raw.githubusercontent.com/Schesch/pension_calculator/main/data/Laborfond.xlsx'
 
     # Read Excel file
     df = pd.read_excel(excel_path, sheet_name=fund_name)
