2fc6bfc3c6790b886eb4d0f1d0ad12dafe204f61
Schesch <57675828+Schesch@users.noreply.github.com>
2024-03-24 15:56:39 +0100

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -13,21 +13,21 @@ sprache = st.selectbox(
 
 if sprache == "Deutsch":
 
-    st.title("Willkommen")
-    st.subheader("zum Online Tool zur Simulation der Beitragsentwicklung für die regionalen Zusatzrentenfonds in Trentino-Südtirol")
+    st.title("Herzlich Willkommen")
+    st.subheader("zum Online Tool zur Berechnung der Beitragsentwicklung für die regionalen Zusatzrentenfonds in Trentino-Südtirol")
 
-    st.markdown("Dieser Online Simulator ermöglich es Ihnen die drei großen Pensionsfonds in der Region Trentino Südtirol und die Performance der einzelnen Investitionslinien zu vergleichen.")
+    st.markdown("Dieser Online Simulator ermöglich es Ihnen die Pensionsfonds in der Region Trentino Südtirol und die Performance der einzelnen Investitionslinien zu vergleichen.")
     st.markdown("Auf der linken Seite können Sie zu dem jeweiligen Fond wechseln und die Parameter für die Berechnung bestimmen.")
-    st.markdown("Wegen der Veröffentlichungspflicht der Rentenfonds konnten die Daten von den Webseiten der jeweiligen Pensionsfonds selbst entnommen werden.")
+    st.markdown("Aufgrund der Veröffentlichungspflicht der Rentenfonds konnten die Daten von den Webseiten der jeweiligen Pensionsfonds selbst entnommen werden.")
     st.markdown("Dieses Projekt steht in keiner Verbindung zu den Institutionen der Zusatzrente, sondern wurde von einer Privatperson selbst erstellt. Es wird keine Haftung für Fehlinformationen übernommen.")
     st.markdown(
         """Entwickelt von **Alex Laimer**"""
     )
 elif sprache == "Italienisch":
     st.title("Benvenuto")
-    st.subheader("al dashboard online per la simulazione dell'evoluzione dei contributi per i fondi pensione complementari regionali in Trentino Alto-Adige.")
+    st.subheader("al dashboard online online per il calcolo dell'evoluzione dei contributi per i fondi pensione complementari regionali in Trentino-Alto Adige.")
 
-    st.markdown("Questo simulatore online vi permette di confrontare i tre grandi fondi pensione nella regione Trentino Alto Adige e la performance delle singole linee di investimento.")
+    st.markdown("Questo simulatore online vi permette di confrontare i fondi pensione nella regione Trentino Alto Adige e la performance delle singole linee di investimento.")
     st.markdown("Sul lato sinistro, è possibile passare al fondo corrispondente e determinare i parametri per il calcolo.")
     st.markdown("A causa dell'obbligo di pubblicazione dei fondi pensione, i dati sono stati prelevati direttamente dai siti web dei rispettivi fondi pensione.")
     st.markdown("Questo progetto non è collegato alle istituzioni della pensione complementare, ma è stato creato da un privato autonomamente. Non si assume alcuna responsabilità per informazioni errate.")
