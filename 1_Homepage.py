082038819227f1af97fe6d7063a8fda4ed5b0a52
Schesch <57675828+Schesch@users.noreply.github.com>
2024-03-25 11:37:45 +0100

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -19,7 +19,7 @@ if sprache == "Deutsch":
     st.markdown("Dieser Online Simulator ermöglich es Ihnen die Pensionsfonds in der Region Trentino Südtirol und die Performance der einzelnen Investitionslinien zu vergleichen.")
     st.markdown("Auf der linken Seite können Sie zu dem jeweiligen Fond wechseln und die Parameter für die Berechnung bestimmen.")
     st.markdown("Aufgrund der Veröffentlichungspflicht der Rentenfonds konnten die Daten von den Webseiten der jeweiligen Pensionsfonds selbst entnommen werden.")
-    st.markdown("Dieses Projekt steht in keiner Verbindung zu den Institutionen der Zusatzrente, sondern wurde von einer Privatperson selbst erstellt. Es wird keine Haftung für Fehlinformationen übernommen.")
+    st.markdown("Dieses Projekt steht in keiner Verbindung zu den Institutionen der Zusatzrente, sondern wurde von einer Privatperson selbst erstellt. Es wird keine Garantie für die Richtigkeit der Informationen gewährt, da die Daten kopiert wurden.")
     st.markdown(
         """Entwickelt von **Alex Laimer**"""
     )
@@ -30,7 +30,7 @@ elif sprache == "Italienisch":
     st.markdown("Questo simulatore online vi permette di confrontare i fondi pensione nella regione Trentino Alto Adige e la performance delle singole linee di investimento.")
     st.markdown("Sul lato sinistro, è possibile passare al fondo corrispondente e determinare i parametri per il calcolo.")
     st.markdown("A causa dell'obbligo di pubblicazione dei fondi pensione, i dati sono stati prelevati direttamente dai siti web dei rispettivi fondi pensione.")
-    st.markdown("Questo progetto non è collegato alle istituzioni della pensione complementare, ma è stato creato da un privato autonomamente. Non si assume alcuna responsabilità per informazioni errate.")
+    st.markdown("Questo progetto non è collegato alle istituzioni della pensione complementare, ma è stato creato da un privato autonomamente. Non viene garantita l'accuratezza delle informazioni poiché i dati sono stati copiati..")
     st.markdown(
         """Sviluppato da **Alex Laimer**"""
     )
